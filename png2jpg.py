a8134757725b854c1994343136c64c84219d7b3f
liangzx <3170105151@zju.edu.cn>
2020-04-21 15:35:05 +0800

    temporary

---
+++ png2jpg.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import cv2 as cv
+import os
+
+def png2jpg(PngPath):
+    img = cv.imread(PngPath, 0)
+    w, h = img.shape[::-1]
+    infile = PngPath
+    outfile = os.path.splitext(infile)[0] + ".jpg"
+    img = Image.open(infile)
+    img = img.resize((int(w / 2), int(h / 2)), Image.ANTIALIAS)
+    try:
+        if len(img.split()) == 4:
+            # prevent IOError: cannot write mode RGBA as BMP
+            r, g, b, a = img.split()
+            img = Image.merge("RGB", (r, g, b))
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        else:
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        return outfile
+    except Exception as e:
+        print("ERROR", e)
+
+
+if __name__ == '__main__':
+    png2jpg(argv[1])
